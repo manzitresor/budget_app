101eee063b9a8de9d8bdcf52cec61996dcc38d58
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-17 21:51:44 +0200

    Add validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,7 @@ class Group < ApplicationRecord
     belongs_to :user
     has_many :entity_groups
     has_many :entities, through: :entity_groups
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
