f531a1649c44c9c7b6b056a92986a0d749d01233
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-18 12:05:01 +0200

    Add category home page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
     before_action :authenticate_user!
     def index
-        @group = Group.where(user_id: current_user.id)
+        @groups = Group.where(user_id: current_user.id)
     end
     def new
         @group = Group.new
