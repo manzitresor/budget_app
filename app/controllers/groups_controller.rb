783e76e75c1315520a90f958e5fe70910cba862c
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-18 09:20:15 +0200

    Add create new category page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,19 @@ class GroupsController < ApplicationController
     def new
         @group = Group.new
     end
+    def create
+        @group = Group.new(group_params)
+        @group.user_id = current_user.id
+        if @group.save
+            redirect_to groups_path
+        else
+            render :new
+        end
+    end
+
+    private
+    
+    def group_params
+        params.require(:group).permit(:name,:icon)
+    end
 end
